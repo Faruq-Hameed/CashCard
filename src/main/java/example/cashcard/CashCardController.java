b13ac1d470ca5df57aa60bb267507dbc149f3498
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-12-06 22:03:11 +0100

    added creat api

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,14 +23,19 @@ public class CashCardController {
     @GetMapping("/{cashCardId}") // @GetMapping marks a method as a handler method. GET requests
     private ResponseEntity<CashCard> findById(@PathVariable Long cashCardId) {
         Optional<CashCard> cashCardOptional = this.cashCardRepository.findById(cashCardId);
-        if (cashCardOptional.isPresent()) { //know what to do if present
-            return ResponseEntity.ok(cashCardOptional.get()); //get the item and return it 
-        } 
-        else {
-            return ResponseEntity.notFound().build(); //throw error
+        if (cashCardOptional.isPresent()) { // know what to do if present
+            return ResponseEntity.ok(cashCardOptional.get()); // get the item and return it
+        } else {
+            return ResponseEntity.notFound().build(); // throw error
         }
         // return this.cashCardRepository.findById(cashCardId)
         // .map(ResponseEntity::ok) //if found, return ok response with the item
-        // .orElseGet(() -> ResponseEntity.notFound().build()); //if not found, return not found response
+        // .orElseGet(() -> ResponseEntity.notFound().build()); //if not found, return
+        // not found response
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard() {
+        return null;
     }
 }
