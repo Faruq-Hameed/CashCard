22b45136394d81d30aa81f94ab7b97553e9c3a9f
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-12-12 08:23:26 +0100

    added nerw user roles with userbuilder

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -38,13 +39,22 @@ class SecurityConfig {
 
     @Bean
     UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.builder() // build a user for in-memory authentication
+        UserBuilder userBuilder = User.builder(); // a builder for UserDetails objects
+        UserDetails sarah = userBuilder // from the builder, build a UserDetails object
                 .username("sarah1") // the username is sarah1
                 .password(passwordEncoder.encode("abc123")) // password is sarah1Pass encoded using BCrypt
-                .roles("USER") // assign the user the role of USER
+                .roles("CARD-OWNER") // assign the user the role of USER
                 .build(); // build the UserDetails object
-        return new InMemoryUserDetailsManager(user); // return an in-memory user details manager with the user we just
-                                                     // built
+
+        UserDetails hankOwnsNoCard = userBuilder
+                .username("hank-owns-no-cards")
+                .password("qrs456")
+                .roles("NON-OWNER")
+                .build();
+
+        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCard); // return an in-memory user details manager with
+                                                                      // the user we just
+        // built
         /*
          * Spring's IoC container will find the UserDetailsService Bean and Spring Data
          * will use it when needed.
